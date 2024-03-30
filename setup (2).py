8e885c927026f58ea9ab7592d54e950bc3f96fc6
dcry2306 <147155497+dcry2306@users.noreply.github.com>
2024-03-30 22:55:08 +0200

    Update setup (2).py

--- setup (2).py
+++ setup (2).py
@@ -1,15 +1,15 @@
 from setuptools import setup
 
 setup(
-    author='CryptoGu1',
-    author_email='Kriptoairdrop9@gmail.com',
-    name='GuDory1',
-    version='1.0.6',
-    description='A simple package for https://app.tea.xyz/. Example GuDory1 - https://github.com/CryptoGu1/GuDory1.git and GuDory2 - https://github.com/CryptoGu1/GuDory2.git',
-    url='https://github.com/CryptoGu1/Dorytea1.git',
+    author='dcry2306',
+    author_email='kilaz2306@gmail.com',
+    name='GuDory',
+    version='0.0.1',
+    description='A simple package for https://app.tea.xyz/. Example pr1 - https://github.com/dcry2306/pr2.git and pr2 - https://github.com/dcry2306/pr2.git',
+    url='https://github.com/CryptoGu1/GuDory.git',
     project_urls={
-        'Homepage': 'https://github.com/CryptoGu1/GuDory1.git',
-        'Source': 'https://github.com/CryptoGu1/GuDory1.git',
+        'Homepage': 'https://github.com/dcry2306/pr2.git',
+        'Source': 'https://github.com/dcry2306/pr2.git',
     },
     py_modules=['hello_tea'],
     entry_points={
